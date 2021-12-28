34038ef2637095e08ee66dc90b85d1aa442a03b3
erom <eromfreitas10@gmail.com>
2021-12-27 21:33:53 -0300

    MirageJS and Axios implementation

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import App from './App';
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 800,
+          type: 'deposit',
+          category: 'Food'
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
